5ba9b21a96c612b0249f0d3d3f0903e9951671fc
Philip Withnall <philip@tecnocode.co.uk>
2008-11-02 12:23:13 +0000

    2008-11-02  Philip Withnall  <philip@tecnocode.co.uk>
    	* packaging/: Added Windows packaging infrastructure and NSIS 
    script.

---
+++ packaging/windows.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Get version
+print "Please enter the MCUS version (e.g. 0.1.1):"
+read version
+
+# Build for Windows
+cd ../
+./autogen.sh --target=i586-mingw32msvc --host=i586-mingw32msvc
+make clean
+make
+
+# Copy the compiled files into our Windows directory tree
+cp src/.libs/mcus.exe packaging/GTK2-Runtime/lib
+cp data/mcus.ui packaging/GTK2-Runtime/lib/data
+cp data/ocr-assembly.lang packaging/GTK2-Runtime/lib/data
+
+# Building zip archive
+zip -r mcus-$version.zip GTK2-Runtime
+
+# Building NSIS installer
+makensis mcus.nsi
